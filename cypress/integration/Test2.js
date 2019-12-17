4ee0c2fd2bb28296d1e644b7a93e58bf00152a9f
Irina O <git@xivic.com>
2019-12-18 01:32:47 +0200

    Iteration

--- cypress/integration/Test2.js
+++ cypress/integration/Test2.js
@@ -15,13 +15,11 @@ describe('Meineke', function(){
         cy.visit('https://www.meineke.com/locations/ca/lakewood-4034/');
         cy.get('div.container div.row .coupon-item').each(($el, index, $list) => {
             // $el is a wrapped jQuery element
-            if ($el.attr('data-offer-title').toLowerCase === '$29.95 synthetic blend package') {
-            // wrap this element so we can
-            // use cypress commands on it
-            cy.wrap($el).click()
-            } else {
-            // do something else
-            }
+            if ($el.attr('data-offer-title') === '$29.95 Synthetic Blend Package') {
+                // wrap this element so we can
+                // use cypress commands on it
+                cy.wrap($el).contains('Print').click();
+            } 
         })
     })
 })
