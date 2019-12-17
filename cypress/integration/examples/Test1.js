2e1e27b726a76b216eb42e547534768b8f5f40f1
Irina O <git@xivic.com>
2019-12-18 00:15:13 +0200

    Get, Find

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -2,8 +2,14 @@
 
 describe('My First Test', function() {
     it('Does not do much!', function() {
-      expect(true).to.equal(true)
-      cy.visit('https://www.meineke.com/dictionary')
+      expect(true).to.equal(true);
+      cy.visit('https://www.meineke.com/dictionary');
+      cy.get('#dictionary-search').type("fuel");
+      cy.wait(2000);
+      cy.get('#dictionary > .btn').click();
+      cy.get('.dictionary-item').should('have.length',4);
+      cy.get('body').find('.dictionary-item').should('have.length',4);
+      cy.get('body').find('.dictionary-item').eq(1).find('h2').should('have.text','Fuel Pumps');
     })
   })
 
