eedc1bf62d96cc53219a27e866ce51ebff7eae75
Irina O <git@xivic.com>
2019-12-18 01:11:38 +0200

    Meineke Tests

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -1,11 +1,13 @@
 /// <reference types="Cypress" />
-
-describe('My First Test', function() {
-    it('Does not do much!', function() {
+/*
+*  Simple asserts on Meineke.com > Dictionary page
+*/
+describe('Meineke', function() {
+    it('Dictionary Page', function() {
       expect(true).to.equal(true);
-      cy.visit('https://www.meineke.com/dictionary');
+      cy.visit('https://www.meineke.com/dictionary/');
       cy.get('#dictionary-search').type("fuel");
-      cy.wait(2000);
+      cy.wait(1000);
       cy.get('#dictionary > .btn').click();
       cy.get('.dictionary-item').should('have.length',4);
       cy.get('body').find('.dictionary-item').should('have.length',4);
---
+++ cypress/integration/Test2.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+/*
+* Meineke.com - Make This My Meineke 
+*/
+
+describe('Meineke', function(){
+    it('Make This My Meineke',function(){
+        cy.visit('https://www.meineke.com/locations/ca/montebello/');
+        cy.wait(5000);
+        cy.get('[data-storeid="4034"]').contains('Make this my Meineke').click();
+        cy.url().should('eq','https://www.meineke.com/locations/ca/lakewood-4034/');
+        cy.get('.location-set').contains('MY MEINEKE').should('have.length',1);        
+    })
+    it('Print Coupon',function(){ 
+        cy.visit('https://www.meineke.com/locations/ca/lakewood-4034/');
+        cy.get('div.container div.row .coupon-item').each(($el, index, $list) => {
+            // $el is a wrapped jQuery element
+            if ($el.attr('data-offer-title').toLowerCase === '$29.95 synthetic blend package') {
+            // wrap this element so we can
+            // use cypress commands on it
+            cy.wrap($el).click()
+            } else {
+            // do something else
+            }
+        })
+    })
+})
