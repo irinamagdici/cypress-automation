6b55fe12cee40bfe377993f8969608b071f6df03
Irina O <git@xivic.com>
2019-12-17 23:20:51 +0200

    cypress.json config

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,9 @@
+/// <reference types="Cypress" />
+
 describe('My First Test', function() {
     it('Does not do much!', function() {
       expect(true).to.equal(true)
-      cy.visit('https://www.meineke.com')
+      cy.visit('https://www.meineke.com/dictionary')
     })
   })
 
